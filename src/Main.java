907e166fb2f27e8b8aeef581d1ea242455b33c81
Vyacheslav <prokman@ya.ru>
2024-08-30 00:05:51 +0600

    реализовано ТЗ4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,94 @@
+import Model.Epic;
+import Model.Status;
+import Model.SubTask;
+import Model.Task;
+import Service.TaskManager;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+
+        TaskManager taskManager = new TaskManager();
+
+        Task task1= new Task("Task1","TDescript1", Status.NEW);
+        Task task2= new Task("Task2","TDescript2", Status.NEW);
+        Task task3= new Task("Task3","TDescript3", Status.NEW);
+
+        Epic epic1=new Epic("Epic1", "EDescript1",Status.NEW);
+        Epic epic2=new Epic("Epic2", "EDescript2",Status.NEW);
+        Epic epic3=new Epic("Epic3", "EDescript3",Status.NEW);
+
+        SubTask subTask1=new SubTask("subtask1", "StDescript1",Status.NEW,epic1);
+        SubTask subTask2=new SubTask("subtask2", "StDescript2",Status.NEW,epic1);
+        SubTask subTask3=new SubTask("subtask3", "StDescript3",Status.NEW,epic1);
+
+        SubTask subTask4=new SubTask("subtask4", "StDescript4",Status.NEW,epic2);
+        SubTask subTask5=new SubTask("subtask5", "StDescript5",Status.NEW,epic2);
+        SubTask subTask6=new SubTask("subtask6", "StDescript6",Status.NEW,epic2);
+
+        SubTask subTask7=new SubTask("subtask7", "StDescript4",Status.NEW,epic3);
+        SubTask subTask8=new SubTask("subtask8", "StDescript5",Status.NEW,epic3);
+        SubTask subTask9=new SubTask("subtask9", "StDescript6",Status.NEW,epic3);
+        ///////////////////////////////////////////////////////
+        //проверка вывода
+        taskManager.addTask(task1);
+        taskManager.addTask(task2);
+        taskManager.addTask(task3);
+
+        System.out.println();
+        System.out.println("вывод тасков- "+taskManager.getTasks());
+        System.out.println();
+
+        taskManager.addEpic(epic1);
+        taskManager.addEpic(epic2);
+        taskManager.addEpic(epic3);
+
+        System.out.println("вывод епиков- "+taskManager.getEpics());
+        System.out.println();
+
+        taskManager.addSubTask(subTask1);
+        taskManager.addSubTask(subTask2);
+        taskManager.addSubTask(subTask3);
+        System.out.println("вывод сабтасков- "+taskManager.getSubTasks());
+
+        /////////////////////////////////////////////////////////////////
+        //проверка обновления
+        SubTask subTask10=new SubTask("subtask10", "StDescript10",Status.IN_PROGRESS,epic1);
+        taskManager.addSubTask(subTask10);
+        System.out.println();
+        System.out.println("вывод обновленных епиков (1 стал прогрес)- "+taskManager.getEpics());
+
+        subTask1=new SubTask("subtask1up", "StDescript1up",Status.DONE, 6, epic1);
+        subTask2=new SubTask("subtask2up", "StDescript2up",Status.DONE, 7, epic1);
+        subTask3=new SubTask("subtask3up", "StDescript3up",Status.DONE, 8, epic1);
+
+        taskManager.updateSubTask(subTask1);
+        taskManager.updateSubTask(subTask2);
+        taskManager.updateSubTask(subTask3);
+        taskManager.removeSubTasksById(9);//удалем 10 сабТаск (id-9)
+
+        System.out.println();
+        System.out.println("вывод обновленных епиков - "+taskManager.getEpics());
+
+        System.out.println();
+        System.out.println("вывод обновленных сабтасков - "+taskManager.getSubTasks());
+
+        System.out.println();
+        System.out.println("вывод Сабтасков по епику"+taskManager.getSubTaskByEpic(3));
+
+
+        System.out.println();
+        System.out.println("вывод таск по ид "+taskManager.getTasksById(1));
+        System.out.println();
+        System.out.println("вывод эпик по ид "+taskManager.getEpicsById(3));
+        System.out.println();
+        System.out.println("вывод Сабтасков по ид "+taskManager.getSubTasksById(7));
+
+        taskManager.clearTasks();
+        taskManager.clearEpics();
+        System.out.println("очистка");
+        System.out.println("вывод тасков- "+taskManager.getTasks());
+        System.out.println("вывод епиков- "+taskManager.getEpics());
+        System.out.println("вывод сабтасков- "+taskManager.getSubTasks());
     }
 }
