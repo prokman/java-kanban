afb0ccd2ec44b1b1aa75d9236a0da5cb5e5548b9
Vyacheslav <prokman@ya.ru>
2024-08-31 12:05:04 +0600

    реализовано ТЗ 4 (Устранены замечания первого ревью+удалил неиспользуемый метод)

--- src/model/Epic.java
+++ src/model/Epic.java
@@ -6,12 +6,10 @@ import java.util.ArrayList;
 public class Epic extends Task {
     private final ArrayList<Integer> ListSubTasksId;
 
-
-
     public Epic(String name, String description, Status statusOfEpic) {
         super(name, description, statusOfEpic);
         //this.subTasksId = new HashMap<>();
-        this.ListSubTasksId =new ArrayList<>();
+        this.ListSubTasksId = new ArrayList<>();
     }
 
     public ArrayList<Integer> getListSubTasksId() {
@@ -20,17 +18,9 @@ public class Epic extends Task {
 
     public void addSubTasks(Integer subTasksId) {
         this.ListSubTasksId.add(subTasksId);
-//                put(subTask.getId(), subTask);
     }
 
     public void removeSubTasks(Integer subTasksId) {
         this.ListSubTasksId.remove(subTasksId);
     }
-
-    public void updateSubTasks(Integer subTasksId) {
-        int indexOfsubTasksId=this.ListSubTasksId.indexOf(subTasksId);
-        this.ListSubTasksId.set(indexOfsubTasksId,subTasksId);
-
-    }
-
 }
