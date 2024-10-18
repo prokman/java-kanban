e73d0ddc82aeff23346790f634fce877c4afccb3
Vyacheslav <prokman@ya.ru>
2024-10-18 23:03:23 +0600

    реализовано ТЗ 7_6

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -5,6 +5,7 @@ public class Managers {
     public static TaskManager getDefaultNoFile() {
         return new InMemoryTaskManager(new InMemoryHistoryManager());
     }
+
     public static TaskManager getDefault() {
         //return new FileBackedTaskManager(new InMemoryHistoryManager(), "java-kanban-data.csv");
         return FileBackedTaskManager.loadFromFile("java-kanban-data.csv");
