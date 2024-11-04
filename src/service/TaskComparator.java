a3bfdc00ef1a7cb0e9913c58ee09b295a8a3ce2a
Vyacheslav <prokman@ya.ru>
2024-11-04 19:07:20 +0600

    реализовано ТЗ 8_3 (учтены замечания ревью, дополнительные правки код-стайл)

--- src/service/TaskComparator.java
+++ src/service/TaskComparator.java
@@ -9,9 +9,9 @@ public class TaskComparator implements Comparator<Task> {
     @Override
     public int compare(Task o1, Task o2) {
         if (o1.getStartTime().isEqual(o2.getStartTime())) {
-            return o1.getId()-o2.getId();
+            return o1.getId() - o2.getId();
         } else {
-            return  o1.getStartTime().compareTo(o2.getStartTime());
+            return o1.getStartTime().compareTo(o2.getStartTime());
         }
     }
 }
